c267e6e91293b8dc44a607bedf5023c56bfa19d3
Burnallover <44618082+Burnallover@users.noreply.github.com>
2023-12-30 18:47:06 +0100

    Create config.h
    added a file config to ease the update of the main code

---
+++ config.h
@@ -0,0 +1,15 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "ssid wifi";  // Mettre votre SSID Wifi
+const char* password = "wifi password";  // Mettre votre mot de passe Wifi
+
+const char* mqttServer = "192.168.XXX.XXX"; // Mettre l'IP du serveur MQTT
+const int mqttPort = 1883;
+const char* mqttUsername = "mqttUsername"; // Mettre le user MQTT
+const char* mqttPassword = "mqttPassword"; // Mettre votre mot de passe MQTT
+
+uint8_t network_id[] = {0xNN, 0xNN, 0xNN, 0xNN}; // Remplacer NN par le network id de la chaudière
+
+#endif // CONFIG_H
