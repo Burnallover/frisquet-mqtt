75caea51aa5849e8e1bd9763b650696da231dcee
Burnallover <44618082+Burnallover@users.noreply.github.com>
2024-02-14 17:20:36 +0100

    new def network id to manage auto association.

--- config.h
+++ config.h
@@ -9,7 +9,11 @@ const char* mqttServer = "192.168.XXX.XXX"; // Mettre l'IP du serveur MQTT
 const int mqttPort = 1883;
 const char* mqttUsername = "mqttUsername"; // Mettre le user MQTT
 const char* mqttPassword = "mqttPassword"; // Mettre votre mot de passe MQTT
+uint8_t def_Network_id[] = {0xFF, 0xFF, 0xFF, 0xFF};
+//Future release
+const bool sensorZ2 = false;
 
-uint8_t network_id[] = {0xFF, 0xFF, 0xFF, 0xFF}; // Remplacer NN par le network id de la chaudière
-
+// network id & ext.sonde id if they are known
+uint8_t network_id[] = {0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t extSon_id = 0xFF;
 #endif // CONFIG_H
